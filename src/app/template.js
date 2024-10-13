abc2fc0c1725fc588ed6af16596ed8636b040576
serhiibeznisko <beznisko.ss@gmail.com>
2024-10-13 09:22:21 +0200

    commit

--- src/app/template.js
+++ src/app/template.js
@@ -2,8 +2,13 @@
 import {TonConnectUIProvider} from '@tonconnect/ui-react';
 import Header from "./components/Header";
 import MobileNav from "./components/MobileNav";
+import WebApp from '@twa-dev/sdk'
+import {useEffect} from "react";
 
 export default function RootTemplate({ children }) {
+    useEffect(() => {
+        WebApp.expand();
+    }, []);
     return (
         <TonConnectUIProvider manifestUrl={'https://main.d1h490shf3nbdl.amplifyapp.com/tonconnect-manifest.json'}>
             <Header/>
