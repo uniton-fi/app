4637c38e65690529db792e153286116b0770b5ed
serhiibeznisko <beznisko.ss@gmail.com>
2024-10-13 11:06:41 +0200

    commit

--- src/app/Swap.js
+++ src/app/Swap.js
@@ -1,8 +1,34 @@
-import React from "react";
+import React, {useState} from "react";
 import styles from "./Swap.module.css";
 import Image from "next/image";
+import {useSwap} from "./useSwap";
+
+const rate = 5.3
 
 const Swap = () => {
+    const {confirmSwap} = useSwap();
+    const [amount, setAmount] = useState('0');
+    const [receive, setReceive] = useState('0');
+
+    function isFloat(val) {
+        return /^[0-9]+(\.)?[0-9]*$/.test(val)
+    }
+
+    const round = (num) => {
+        return Math.round(num * 100) / 100
+    }
+
+    const onAmountChange = (e) => {
+        var newAmount = e.target.value;
+        if(newAmount === "") {
+            newAmount = "0";
+        } else if(!isFloat(newAmount)) {
+            return;
+        }
+        setAmount(parseFloat(newAmount));
+        setReceive(round(parseFloat(newAmount) * rate))
+    }
+
     return (
         <div className={styles.swapContainer}>
             <p className={styles.swapTitle}>Swap</p>
@@ -12,13 +38,16 @@ const Swap = () => {
                     balance={'70.42'}
                     tokenIcon={'/ton.png'}
                     tokenName={'TON'}
+                    value={amount}
+                    onChange={onAmountChange}
                 />
                 <SwapInput
                     title={'You get'}
-                    balance={'70.42'}
+                    balance={'5.20'}
                     tokenIcon={'/usdt.png'}
                     tokenName={'USDT'}
                     readonly={'readonly'}
+                    value={receive}
                 />
                 <div className={styles.exchangeIcon}>
                     <Image src={'/filter-exchange.svg'} alt={'change'} width="24" height="24" />
@@ -28,24 +57,24 @@ const Swap = () => {
                 <div className={styles.markContainer}>
                     <Image src={'/marked.svg'} alt={'change'} width="9" height="9" />
                 </div>
-                <span className={styles.detailsText}>1 TON = 1.4455 STON($3.34)</span>
+                <span className={styles.detailsText}>1 TON = {rate} USDT(${rate})</span>
                 <div className={styles.detailsExpand}>
                 <Image src={'/expand.svg'} alt={'change'} width="20" height="20" />
                 </div>
             </div>
-            <button className={styles.swapButton}>Swap</button>
+            <button className={styles.swapButton} onClick={() => confirmSwap(amount)}>Swap</button>
         </div>
     );
 };
 
-const SwapInput = ({title, balance, tokenIcon, tokenName, readonly}) => {
+const SwapInput = ({title, balance, tokenIcon, tokenName, readonly, value, onChange}) => {
     return (
         <div className={styles.swapInputContainer}>
             <p className={styles.swapInputTitle}>{title}</p>
             <div className={styles.swapInputLine}>
                 <Image src={tokenIcon} alt={tokenName} width="40" height="40" />
                 <span className={styles.swapTokenTitle}>{tokenName}</span>
-                <input type="text" className={styles.swapInputField} readOnly={readonly} />
+                <input type="text" className={styles.swapInputField} readOnly={readonly} value={value} onChange={onChange} />
             </div>
             <div className={styles.swapBalance}>
                 <span>Balance:</span>
--- src/app/useSwap.js
+++ src/app/useSwap.js
@@ -1,43 +1,39 @@
-import { Factory, MAINNET_FACTORY_ADDR, Asset, PoolType } from '@dedust/sdk';
+import { Factory, MAINNET_FACTORY_ADDR, Asset, PoolType, VaultNative, Vault } from '@dedust/sdk';
 import { Address, TonClient4, beginCell } from "@ton/ton";
 import { toNano } from '@ton/core';
-import {useTonConnectUI} from "@tonconnect/ui-react";
+import {useTonConnectUI, useTonAddress} from "@tonconnect/ui-react";
 
 
 export const useSwap = () => {
     const [tonConnectUI, setOptions] = useTonConnectUI();
+    const val = useTonAddress();
 
-    const confirmSwap = async () => {
-        const tonClient = new TonClient4({ endpoint: "https://mainnet-v4.tonhubapi.com" });
-        const factory = tonClient.open(Factory.createFromAddress(MAINNET_FACTORY_ADDR));
-        const tonVault = tonClient.open(await factory.getNativeVault());
+    const confirmSwap = async (amount, limit=0) => {
+        const poolAddress = Address.parse('EQA-X_yo3fzzbDbJ_0bzFWKqtRuZFIRa1sJsveZJ1YpViO3r');
+        const cell = beginCell()
+            .storeUint(VaultNative.SWAP, 32)
+            .storeUint(0, 64)
+            .storeCoins(toNano(amount))
+            .storeAddress(poolAddress)
+            .storeUint(0, 1)
+            .storeCoins(limit ?? 0)
+            .storeRef(Vault.packSwapParams({}))
+            .endCell()
+        const boc = cell.toBoc().toString('hex');
 
-        const TOKEN_ADDRESS = Address.parse('EQCxE6mUtQJKFnGfaROTKOt1lZbDiiX1kCixRv7Nw2Id_sDs');
-        const TOKEN1 = Asset.native();
-        const TOKEN2 = Asset.jetton(TOKEN_ADDRESS);
+        const sendAmount = toNano(amount) + toNano('0.2');
+        const myTransaction = {
+            validUntil: Math.floor(Date.now() / 1000) + 60, // 60 sec
+            messages: [
+                {
+                    address: poolAddress.toString(),
+                    amount: sendAmount.toString(),
+                    payload: boc
+                }
+            ]
+        }
 
-        const pool = tonClient.open(await factory.getPool(PoolType.VOLATILE, [TOKEN1, TOKEN2]));
-        const amountIn = toNano('5');
-        await tonVault.sendSwap(tonClient, {
-            poolAddress: pool.address,
-            amount: amountIn,
-            gasAmount: toNano("0.25"),
-        });
-
-        console.log(pool)
-
-        // const myTransaction = {
-        //     validUntil: Math.floor(Date.now() / 1000) + 60, // 60 sec
-        //     messages: [
-        //         {
-        //             address: "UQD0wDonlezaxX_N6nD4LB78vsGyVNiiJfQVwu_GeLUMPbVD",
-        //             amount: "1000000000",
-        //             // stateInit: "base64bocblahblahblah==" // just for instance. Replace with your transaction initState or remove
-        //         }
-        //     ]
-        // }
-        //
-        // await tonConnectUI.sendTransaction(myTransaction);
+        await tonConnectUI.sendTransaction(myTransaction);
     }
 
     return {
